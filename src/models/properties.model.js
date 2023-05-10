47a0e51349bea56b8bac04cbd871d67b0f36dc11
Stephen <slulego@live.com>
2023-05-10 09:30:21 -0400

    Remove built files

--- src/models/properties.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Direction = void 0;
-var Direction;
-(function (Direction) {
-    Direction["Vertical"] = "column";
-    Direction["Horizontal"] = "row";
-})(Direction = exports.Direction || (exports.Direction = {}));
