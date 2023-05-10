d2e0df1c4bd9a267aa778680c158ce82878c07f5
Stephen <slulego@live.com>
2023-05-10 09:26:32 -0400

    Fix gitignore

--- dist/models/properties.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Direction = void 0;
-var Direction;
-(function (Direction) {
-    Direction["Vertical"] = "column";
-    Direction["Horizontal"] = "row";
-})(Direction = exports.Direction || (exports.Direction = {}));
