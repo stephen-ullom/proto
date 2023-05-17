8ff71309daad67ae707ea12bce730c2bd081fbfe
Stephen <slulego@live.com>
2023-05-17 00:27:07 -0400

    Fix repeat callback

--- src/components/repeat.ts
+++ src/components/repeat.ts
@@ -2,7 +2,7 @@ import { Content } from "../models/html.model";
 
 export function repeat(
   array: any[],
-  callback: (value?: any, index?: number, array?: any[]) => Content
+  callback: (value: any, index: number, array: any[]) => Content
 ) {
   return array.map(callback).join("");
 }
