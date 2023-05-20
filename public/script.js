9feec9b72a911c89817748bf9d9c2343372c543f
Stephen <slulego@live.com>
2023-05-20 10:39:37 -0400

    Make content draggable

--- public/script.js
+++ public/script.js
@@ -1,16 +1,34 @@
-let mainElement;
+let container;
+let content;
 let zoomLevel = 1;
+
+let initialMouseX = 0;
+let initialMouseY = 0;
+let initialContainerX = 0;
+let initialContainerY = 0;
+let grabbing = false;
+let spacebarPressed = false;
 
 function init() {
   connect();
-  mainElement = document.getElementById("proto");
+  container = document.getElementById("draggable-container");
+  content = document.getElementById("content");
+
+  document.addEventListener("keydown", keydown);
+  document.addEventListener("keyup", keyup);
+
+  if (container) {
+    container.addEventListener("mousedown", startDragging);
+    container.addEventListener("mousemove", drag);
+    container.addEventListener("mouseup", stopDragging);
+  }
 }
 
 function connect() {
   const socket = new WebSocket("ws://localhost:2000");
 
   socket.addEventListener("message", (event) => {
-    if (mainElement) mainElement.innerHTML = event.data;
+    if (content) content.innerHTML = event.data;
     socket.send("Preview updated.");
   });
 
@@ -21,15 +39,68 @@ function connect() {
 
 function resetZoom() {
   zoomLevel = 1;
-  mainElement.style.zoom = zoomLevel;
+  container.style.zoom = zoomLevel;
 }
 
 function zoomIn() {
   zoomLevel += 0.1;
-  mainElement.style.zoom = zoomLevel;
+  container.style.zoom = zoomLevel;
 }
 
 function zoomOut() {
   zoomLevel -= 0.1;
-  mainElement.style.zoom = zoomLevel;
+  container.style.zoom = zoomLevel;
+}
+
+function keydown(event) {
+  if (event.key === " ") {
+    event.preventDefault();
+    spacebarPressed = true;
+    setCursor();
+  }
+}
+
+function keyup(event) {
+  if (event.key === " ") {
+    spacebarPressed = false;
+    setCursor();
+  }
+}
+
+function startDragging(event) {
+  if (spacebarPressed) {
+    grabbing = true;
+    setCursor();
+    initialMouseX = event.clientX;
+    initialMouseY = event.clientY;
+    initialContainerX = content.offsetLeft * zoomLevel;
+    initialContainerY = content.offsetTop * zoomLevel;
+  }
+}
+
+function drag(event) {
+  if (!grabbing) return;
+  event.preventDefault();
+  const offsetX = event.clientX - initialMouseX;
+  const offsetY = event.clientY - initialMouseY;
+  const left = initialContainerX + offsetX;
+  const top = initialContainerY + offsetY;
+  const multiplyer = 1 / zoomLevel;
+  content.style.left = `${left * multiplyer}px`;
+  content.style.top = `${top * multiplyer}px`;
+}
+
+function stopDragging() {
+  grabbing = false;
+  setCursor();
+}
+
+function setCursor() {
+  if (grabbing) {
+    container.style.cursor = "grabbing";
+  } else if (spacebarPressed) {
+    container.style.cursor = "grab";
+  } else {
+    container.style.cursor = "default";
+  }
 }
